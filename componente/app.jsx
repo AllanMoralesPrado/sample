63d72a9d035f47351c53f84c9d8294ec11068373
jotakf <jpcisternaskf@gmail.com>
2024-03-06 12:23:20 -0300

    ahjsbajkd

---
+++ componente/app.jsx
@@ -0,0 +1,30 @@
+import NavBar from "./componentes/NavBar";
+import Footer from "./componentes/Footer/footer";
+import CicloVital from "./pages/CicloVital/CicloVital";
+import Index from "./pages/Index/Index";
+import Popup from "./componentes/Popup/Popup";
+import ScrollList from "./componentes/ScrollList/ScrollList";
+import Examenes from "./pages/Examenes/Examenes";
+import Localizacion from "./pages/Localizacion/Localizacion";
+
+function App() {
+  return (
+    <>
+      <header className="header">
+        <NavBar />
+      </header>
+
+      <main className="main">
+        {/* <CicloVital /> */}
+        {/* <Index /> */}
+        <Popup />
+        {/* <Examenes /> */}
+        {/* <Localizacion /> */}
+      </main>
+
+      <Footer />
+    </>
+  );
+}
+
+export default App;
